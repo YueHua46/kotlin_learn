410eb3fb6532e1fb0f9d6cf06988f76736e6f52c
shengxi <2766274062@qq.com>
2023-08-11 13:12:53 +0800

    [feat]: 更新布局方式

--- Article/app/src/main/java/com/example/article/MainActivity.kt
+++ Article/app/src/main/java/com/example/article/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -63,7 +64,6 @@ fun GreetingTitle(text: String) {
         text = text,
         fontSize = 26.sp,
         textAlign = TextAlign.Justify,
-        modifier = Modifier.padding(16.dp)
     )
 }
 
@@ -75,7 +75,7 @@ fun GreetingText(text: String) {
         // 文本的对齐方式
         // TextAlign.Justify 表示文本应该两端对齐，也就是说，文本的左右两端都应该与其容器的边缘对齐。
         textAlign = TextAlign.Justify,
-        modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        // modifier = Modifier.padding(start = 16.dp, end = 16.dp)
     )
 }
 
@@ -85,7 +85,11 @@ fun GreetingPreview() {
     ArticleTheme {
         Column {
             GreetingImage(R.drawable.bg_compose_background, dsc = "banner图")
-            Column {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                // 使用Arrangement.spacedBy方法来设置元素内之间的间距，类似于css中gap效果
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
                 GreetingTitle(stringResource(R.string.blog_title))
                 GreetingText(stringResource(R.string.blog_content1))
                 GreetingText(stringResource(R.string.blog_content2))
